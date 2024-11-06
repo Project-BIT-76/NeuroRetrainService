eef1775f2fea80afd4213265009771c7b992ddd2
Никита <nikata.shatalov.04@mail.ru>
2024-11-06 09:44:34 +0300

    update start project

---
+++ src/test/java/project/bit/bit/BitApplicationTests.java
@@ -0,0 +1,13 @@
+package project.bit.bit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
